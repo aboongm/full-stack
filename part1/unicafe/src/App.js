1c4abf14846c3f7786a3d601894e327566a2c378
aboongm <aboongm@yahoo.com>
2022-04-27 20:08:16 +0530

    Add the statistics and the feedbacks etc

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,7 +5,23 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <div>
+        <button>good</button>
+        <button>neutral</button>
+        <button>bad</button>
+      </div>
+      <h1>statistics</h1>
+      <p>good 6</p>
+      <p>neutral 2</p>
+      <p>bad 2</p>
+      <p>all 9</p>
+      <p>average 0.55%</p>
+      <p>positive 66.5%</p>
+    </div>
+  );
 };
 
 export default App;
