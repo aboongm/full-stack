7f8d75849003c0ed07dcc629fbd4494e909108be
aboongm <aboongm@yahoo.com>
2022-04-27 17:14:17 +0530

    turn the names and exercises into objects

--- part1/courseinfo/src/Part.js
+++ part1/courseinfo/src/Part.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Part = (props) => {
+  // console.log(props.part.name);
   return (
     <div>
       <p>
-        {props.part} {props.exercise}
+        {props.part.name} {props.part.exercises}
       </p>
     </div>
   );
