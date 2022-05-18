5dc8d4ddeea264307e036b57f048f3104450ff95
aboongm <aboongm@yahoo.com>
2022-05-18 17:23:00 +0530

    Completed part 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 
+import axios from 'axios';
+
+// axios.get('http://localhost:3001/persons').then((response) => {
+//   const notes = response.data;
+//   console.log(notes);
+// });
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filterNames, setFilterNames] = useState('');
+
+  useEffect(() => {
+    const eventHandler = (response) => {
+      setPersons(response.data);
+    };
+    const promise = axios.get('http://localhost:3001/persons');
+    promise.then(eventHandler);
+  }, []);
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Persons = ({ filtered }) => {
+  console.log(filtered);
   return (
     <>
       {filtered.map((person, index) => (
